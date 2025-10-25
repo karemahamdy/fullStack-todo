501a6d05ad5df5ca6938e868f23e58df5b6b4801
Karema <karemahamdy51@gmail.com>
2025-10-26 01:18:52 +0300

    add form validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import {
@@ -11,16 +17,47 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { getTodos } from "@/actions/todo.actions";
+// import { getTodos } from "@/actions/todo.actions";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 
-export default async function Home() {
-  const todos = await getTodos();
+const formSchema = z.object({
+  title: z.string().min(2, {
+    message: "title must be at least 2 characters.",
+  }),
+  body: z.string().min(2, {
+    message: "body must be at least 2 characters.",
+  }),
+})
+
+export default  function Home() {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: { title: "", body: "" },
+  })
+
+  // ✅ handle submit
+  function onSubmit(values  : z.infer<typeof formSchema>) {
+    console.log("Submitted:", values)
+  
+  }
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-3xl font-bold">Welcome to Next.js!</h1>
-        <pre>{JSON.stringify(todos, undefined, 2)}</pre>
+        {/* <ul>
+          {todos.map(todo => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul> */}
+        {/* <pre>{JSON.stringify(todos, undefined, 2)}</pre> */}
         <Dialog>
           <form>
             <DialogTrigger asChild>
@@ -37,21 +74,50 @@ export default async function Home() {
                 </DialogDescription>
               </DialogHeader>
               <div className="grid gap-4">
-                <div className="grid gap-3">
-                  <Label htmlFor="name-1">Name</Label>
-                  <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
-                </div>
-                <div className="grid gap-3">
-                  <Label htmlFor="username-1">Username</Label>
-                  <Input id="username-1" name="username" defaultValue="@peduarte" />
-                </div>
+                <Form {...form}>
+                  <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                      control={form.control}
+                      name="title"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Username</FormLabel>
+                          <FormControl>
+                            <Input placeholder="shadcn" {...field} />
+                          </FormControl>
+                          <FormDescription>
+                            This is your public display name.
+                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="body"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>body</FormLabel>
+                          <FormControl>
+                            <Input placeholder="shadcn" {...field} />
+                          </FormControl>
+                          <FormDescription>
+                            This is your public display name.
+                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <DialogFooter>
+                      <DialogClose asChild>
+                        <Button variant="outline">Cancel</Button>
+                      </DialogClose>
+                      <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                  </form>
+                  </Form>
               </div>
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button variant="outline">Cancel</Button>
-                </DialogClose>
-                <Button type="submit">Save changes</Button>
-              </DialogFooter>
+            
             </DialogContent>
           </form>
         </Dialog>
