af5c7cdda98a0faa39a3092f55b6ac515a294cbf
Karema <karemahamdy51@gmail.com>
2025-11-02 20:57:04 +0200

    delete user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -13,19 +13,7 @@ async function main() {
             }),
       ),
     })                          
-  await prisma.user.createMany({
-      data: Array.from({ length: 30 }, () => ({
-          email: faker.internet.email(),
-          name: faker.internet.username(),
-          address: {
-            street: faker.location.street(),
-            city: faker.location.city(),
-            state: faker.location.state(),
-            zip: faker.location.zipCode(),
-        },
-      }),
-      ),
-    })
+
 }
 main()
   .catch(async (e) => {
